731505a600830cb498bb513b42e30bfe80ab8814
simon <simonmartinez1605@gmail.com>
2023-12-22 18:28:48 -0500

    comienzo de filtros por categoria

--- paginaPrincipal/catalogo.js
+++ paginaPrincipal/catalogo.js
@@ -37,7 +37,8 @@ const addDataToHTMl = () =>{
 
             //Mostrar los productos de la api en el html 
             newProduct.innerHTML = `
-            <div id= "product"> 
+            
+            <div id= "product" class="dad-card"> 
                 <div class="card" style="width: 18rem; height: 24rem;" data-id="${product.ID}" data-price="${product.Precio_Mayorista}" data-referencia="${product.Referencia}" data-imagen="${product.Imagen_publica.url}"> 
                     <div class="card-body"> 
                         <img src="${product.Imagen_publica.url}" class="card-img-top" alt="...">  
@@ -169,7 +170,7 @@ const addCartToHTML = () =>{
                         </div>
                         <div class="container-borrar-carrito">
 
-                            <button class="icon-trash">
+                            <button class="container-icon-trash">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="icon-trash" viewBox="0 0 16 16">
                                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
                                 <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
@@ -205,7 +206,7 @@ listCartHTML.addEventListener('click', (event) =>{
                 type = 'plus'; 
             }
             if(positionClick.classList.contains('icon-trash')){
-                console.log('hola')
+                type = 'delete'
             }
             //funcion para cambiar cantidad
             changeQuantity(product_id, type)
@@ -223,7 +224,18 @@ const changeQuantity = (product_id, type) =>{
             case 'plus': 
                 carts[positionItemInCart].quantity = carts[positionItemInCart].quantity + 1; 
                 break; 
-            
+            //Funcion para eliminar del carrito 
+            case 'delete': 
+                carts[positionItemInCart].quantity = carts[positionItemInCart].quantity ==0;   
+                let value = carts[positionItemInCart].quantity ==0; 
+                console.log(value)
+                if(value == true){
+                    carts[positionItemInCart],quantity = value; 
+                }
+                else{
+                    carts.splice(positionItemInCart, 1)
+                }
+
             default: 
                 let valueChange = carts[positionItemInCart].quantity -1; 
                 if(valueChange >0){
@@ -270,6 +282,24 @@ const optionMenu = document.querySelector('.select-menu'),
         sBtn_text = optionMenu.querySelector('.sBtn-text');  
     
     selectBtn.addEventListener('click', () => optionMenu.classList.toggle('active')); 
+
+
+    selectBtn.addEventListener('click', () =>{
+        var filtro = listProducts; 
+        const found = filtro.filter(product =>{
+            return filtro; 
+        }); 
+
+        if(found){
+            console.log(found)
+            let Caracteristicas = found.filter((e)=>{
+                
+            });
+        }
+        else{
+            console.log('no')
+        }
+    })
     
     options.forEach(option => {
         option.addEventListener('click', ()=>{
@@ -279,7 +309,7 @@ const optionMenu = document.querySelector('.select-menu'),
             console.log(selectOption)
             optionMenu.classList.remove('active');  
 
-        })
+    })
 }); 
 
 const btnCart = document.querySelector('.container-cart-icon')  
@@ -296,7 +326,7 @@ const inputBuscar = document.getElementById('buscar')
 
 const cards = document.getElementsByTagName('h5')
 
-const dad = document.getElementsByClassName('card-body') 
+const dad = document.getElementsByClassName('dad-card') 
 
 inputBuscar.addEventListener('keyup', (e) =>{
     let text = e.target.value
@@ -306,7 +336,7 @@ inputBuscar.addEventListener('keyup', (e) =>{
     for(let i= 0; i<cards.length ; i++){  
         let valor = cards[i] 
         let container = dad[i]  
-        // console.log(valor) 
+        //console.log(container) 
         if (search.test(valor.innerText)){
             valor.classList.remove('cards')  
             container.classList.remove('dads')
@@ -326,14 +356,6 @@ const containerCart = document.querySelector('.container-cart')
 
 btnCloseCart.addEventListener('click', ()=>{
     containerCart.classList.toggle('hidden-cart') 
-}) 
+}); 
 
-//Funcion para vaciar el carrito 
-// document.addEventListener('DOMContentLoaded', function(){
-//     const vaciarCarrito = document.querySelector('.icon-trash'); 
-//     const containerProduct = document.querySelector('.info-cart-product'); 
 
-//     vaciarCarrito.addEventListener('click', ()=>{
-//         console.log(containerProduct) 
-//     }); 
-// }); 
