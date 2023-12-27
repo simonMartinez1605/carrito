f95ee95b427abcaf6b9fde2c5eb2a933f3b06a86
simon <simonmartinez1605@gmail.com>
2023-12-27 13:59:08 -0500

    actualizacion

--- js/botones.js
+++ js/botones.js
@@ -3,50 +3,50 @@
 
 //Mostrar y ocultar el menu de categoria 
 
-const optionMenu = document.querySelector('.select-menu'), 
-        selectBtn = optionMenu.querySelector('.select-btn'), 
-        options = optionMenu.querySelectorAll('.option'), 
-        sBtn_text = optionMenu.querySelector('.sBtn-text');  
+// const optionMenu = document.querySelector('.select-menu'), 
+//         selectBtn = optionMenu.querySelector('.select-btn'), 
+//         options = optionMenu.querySelectorAll('.option'), 
+//         sBtn_text = optionMenu.querySelector('.sBtn-text');  
     
-    selectBtn.addEventListener('click', () => optionMenu.classList.toggle('active')); 
+//     selectBtn.addEventListener('click', () => optionMenu.classList.toggle('active')); 
 
     
-    options.forEach(option => {
-        option.addEventListener('click', ()=>{
-            const selectOption = option.querySelector('.option-text').innerText; 
-            sBtn_text.innerText = selectOption; 
+//     options.forEach(option => {
+//         option.addEventListener('click', ()=>{
+//             const selectOption = option.querySelector('.option-text').innerText; 
+//             sBtn_text.innerText = selectOption; 
     
-            //console.log(selectOption)
-            optionMenu.classList.remove('active');  
+//             //console.log(selectOption)
+//             optionMenu.classList.remove('active');  
 
 
-            // sBtn_text.addEventListener('click', () =>{
-            //     var filtro = listProducts; 
-            //     const found = filtro.filter(product =>{
-            //         nombre = product.Referencia; 
-            //         //console.log(nombre) 
+//             // sBtn_text.addEventListener('click', () =>{
+//             //     var filtro = listProducts; 
+//             //     const found = filtro.filter(product =>{
+//             //         nombre = product.Referencia; 
+//             //         //console.log(nombre) 
             
-            //         return filtro; 
-            //     }); 
-            //     console.log(selectOption) 
-            //     let text = sBtn_text.target
-            //     console.log(text) 
+//             //         return filtro; 
+//             //     }); 
+//             //     console.log(selectOption) 
+//             //     let text = sBtn_text.target
+//             //     console.log(text) 
             
-            // }); 
-        }); 
+//             // }); 
+//         }); 
 
-    }); 
+//     }); 
 
 
- 
+//Mostrar carrito 
 const btnCart = document.querySelector('.container-cart-icon')  
     const containerCartProducts = document.querySelector('.container-cart') 
     
-    btnCart.addEventListener('click', ()=>{
+    btnCart.addEventListener('click', ()=>{ 
         containerCartProducts.classList.toggle('hidden-cart')  
 }); 
 
-//Funcion para mostrar y cerrar el carrito 
+//Funcion cerrar el carrito 
 const btnCloseCart = document.querySelector('.close')
 const containerCart = document.querySelector('.container-cart')
 
